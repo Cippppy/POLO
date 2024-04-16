48b2454774da2bd19a32bd253d1b112489e7c95d
Christian Cipolletta <cipoll17@students.rowan.edu>
2024-04-15 23:04:47 -0400

    Init

---
+++ examples/segment.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n-seg.pt')  # load an official model
+# model = YOLO('path/to/best.pt')  # load a custom model
+
+# Run inference on 'bus.jpg' with arguments
+model.predict('bus.jpg', save=True)
